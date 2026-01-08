d49c0b7f9677b844a1ccd92e8ae3557b8f2e13a1
Ryan Porter <ryan.porter@deq.oregon.gov>
2026-01-08 14:34:00 -0800

    Added code to calculate PM2.5 AQI

--- src/envista/transformers/transform_env.py
+++ src/envista/transformers/transform_env.py
@@ -7,6 +7,7 @@ into cleaned records with a dataframe schema matching AQS.
 from __future__ import annotations
 from pathlib import Path
 
+from .calculate_aqi import calculate_aqi
 import pandas as pd
 
 def transform_env_df(input_path: Path, unique_monitors: pd.DataFrame) -> pd.DataFrame:
@@ -37,7 +38,7 @@ def transform_env_df(input_path: Path, unique_monitors: pd.DataFrame) -> pd.Data
         "stations_tag":"site_code"})
     
     merged_df["poc"] = "99"
-    merged_df["parameter_code"] = "99999"
+    merged_df["parameter_code"] = "88502"
     merged_df["parameter"] = "Acceptable PM2.5 AQI & Speciation Mass"
     merged_df["sample_duration_code"] = "1"
     merged_df["sample_duration"] = "1 HOUR"
@@ -46,7 +47,13 @@ def transform_env_df(input_path: Path, unique_monitors: pd.DataFrame) -> pd.Data
     merged_df["method_code"] = "999"
     merged_df["method"] = "SensOR PM2.5 Monitor"
     merged_df["aqi"] = pd.NA
+    merged_df["observation_count"] = pd.NA
+    merged_df["observation_percent"] = pd.NA
+    merged_df["first_max_value"] = pd.NA
+    merged_df["first_max_hour"] = pd.NA
 
-    return merged_df[["parameter_code", "poc", "parameter", "sample_duration_code", "sample_duration", 
-                "date_local", "units_of_measure", "event_type", "validity_indicator", 
-                "arithmetic_mean", "aqi", "method_code", "method", "site_code"]]
+    final_df = calculate_aqi(merged_df)
+
+    return final_df[["parameter_code", "poc", "parameter", "sample_duration_code", "sample_duration", 
+                "date_local", "units_of_measure", "event_type", "observation_count", "observation_percent", "validity_indicator", 
+                "arithmetic_mean", "first_max_value", "first_max_hour", "aqi", "method_code", "method", "site_code"]]
